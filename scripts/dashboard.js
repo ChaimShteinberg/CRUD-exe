ce461efb7585dd319cc4dc230298330f119de233
Chaim Shteinberg <a0533197133@gmail.com>
2025-08-19 08:44:49 +0300

    finish time

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,25 +1,53 @@
 const reports = JSON.parse(localStorage.getItem("reports"));
-const article = document.getElementById("reports")
+const article = document.getElementById("reports");
 
-for (let report of reports) {
-
+for (let [index, report] of reports.entries()) {
   const section = document.createElement("section");
+  section.className = "report";
+  section.id = `report${index}`;
 
   const agentName = document.createElement("p");
-  agentName.textContent = 'Agent Name: ' + report.agentName
+  agentName.id = "agentName";
+  agentName.textContent = "Agent Name: " + report.agentName;
 
   const agentCode = document.createElement("p");
-  agentCode.textContent = 'Agent Code: ' + report.agentCode
+  agentCode.id = "agentCode";
+  agentCode.textContent = "Agent Code: " + report.agentCode;
 
   const target = document.createElement("p");
-  target.textContent = 'Target: ' + report.target
+  target.id = "target";
+  target.textContent = "Target: " + report.target;
 
   const information = document.createElement("p");
-  information.textContent = 'Information: ' + report.information
+  information.id = "information";
+  information.textContent = "Information: " + report.information;
 
   const submissionTime = document.createElement("p");
-  submissionTime.textContent = "Submission Time: " + report.submissionTime
+  submissionTime.id = "submissionTime";
+  submissionTime.textContent = "Submission Time: " + report.submissionTime;
 
-  section.append(agentName, agentCode, target, information, submissionTime);
-  article.append(section)
+  const features = document.createElement("section");
+  features.className = "features";
+
+  const edit = document.createElement("button");
+  edit.className = "edit";
+  edit.textContent = "edit report";
+
+  features.append(edit);
+
+  section.append(
+    agentName,
+    agentCode,
+    target,
+    information,
+    submissionTime,
+    features
+  );
+  article.append(section);
 }
+
+article.addEventListener("click", (e) => {
+  if (e.target.className === "edit") {
+    const section = e.target.closest(".report");
+  }
+});
